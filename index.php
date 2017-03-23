7c37ced77a8c61ec8c9747057fc780a8ec25d752
Fanny-Jan <fanny.jan12@gmail.com>
2017-03-23 17:50:00 +0100

    ajout des json utiles, categories en com

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
-    $api = file_get_contents("https://world.openfoodfacts.org/categories.json");
-    json_decode($api);
-var_dump($api);
+
+
+/*  CATEGORIES 
+ *
+ *
+    $api = file_get_contents("https://fr.openfoodfacts.org/categories.json");
+    $json = json_decode($api, true);
+echo '<pre>' . print_r($json, true) . '</pre>';
+   $test = $json['tags'][27]['name'];
+    var_dump($test);
+
+    foreach ($json as $value){
+        foreach ($value as $cat){
+            foreach ($cat as $name){
+                echo $name .'</br>';
+            }
+        }
+    }
+*/
+
+
+
+
+
 
 
 ?>
+
