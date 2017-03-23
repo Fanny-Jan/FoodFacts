91594cabc7f2b1c283168b1fe34f2d07ed47ab0d
HageraN <hagii.neche@gmail.com>
2017-03-23 16:30:31 +0100

    Dossier CSS

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
 <?php
-    $api = file_get_contents("https://world.openfoodfacts.org/categories.json");
-    json_decode($api);
-var_dump($api);
+
+$api = file_get_contents("https://world.openfoodfacts.org/categories.json");
+$json = json_decode($api, true);
+echo '<pre>' . print_r($json, true) . '</pre>';
+
 
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Open Food Facts TEAM</title>
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+    <H1>OPEN FOOD FACTS</H1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="form-group">
+                <form method="post" action="">
+                    
+
+                </form>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
+
+
+
+
+
+
